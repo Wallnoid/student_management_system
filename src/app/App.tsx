daf7ba853f97681eccbaa05e50edb03181425ec4
WilliamPerez2002 <wulli.mu28@gmail.com>
2024-04-21 19:03:43 -0500

    added NextUI

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default function App() {
+  return (
+    <div className="p-4 bg-gray-100 dark:bg-gray-800">
+      <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-200">
+        Hello NextUI
+      </h1>
+    </div>
+  );
+}
