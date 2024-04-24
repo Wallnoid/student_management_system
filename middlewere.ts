b485525943e5acc97d7826589da788bd9b5b53ad
EmeritusThinkPad <jairparedesal123@gmail.com>
2024-04-24 00:11:30 -0500

    auth ready with system pages protected, only available to authenticated users

--- middlewere.ts
+++ middlewere.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from 'next/server'
-import { updateSession } from './utils/supabase/middlewere'
+import { updateSession } from './src/supabase/middlewere'
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request)
