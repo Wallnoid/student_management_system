044895599be37b8587fdee76ff0b90e1b758acb5
WilliamPerez2002 <wulli.mu28@gmail.com>
2024-05-06 21:27:54 -0500

    fixed error alert

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,52 @@
 "use client";
 
 import React, { FormEvent } from "react";
-import { Button, Card, CardBody, Input, Image } from "@nextui-org/react";
+import { Card, CardBody, Image } from "@nextui-org/react";
 import LoginInput from "./components/login_input";
 import LoginSubmit from "./components/login_submit";
 import Alert from "../components/alert";
-import { login } from "./actions";
+import { login, redirectIfAuth } from "./actions";
 
 export default function LoginPage() {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const [error, setError] = React.useState(false);
+  const [errors, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const setPasswordChange = (value: string) => {
+    setPassword(value);
+    setError(false);
+  };
+
+  const setUsernameChange = (value: string) => {
+    setUsername(value);
+    setError(false);
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    setError(false);
+    setLoading(true);
+    console.log("submit");
+
+    if (!username || !password) {
+      setError(true);
+      setLoading(false);
+      return;
+    }
+
+    const data = await login(username, password);
+
+    if (!data) {
+      console.log("error");
+      setError(true);
+      setLoading(false);
+      setPassword("");
+      return;
+    }
+
+    redirectIfAuth("/home");
+  };
 
   return (
     <div className=" flex justify-center items-center h-screen">
@@ -46,13 +82,13 @@ export default function LoginPage() {
                 <span className="border-b w-1/5 lg:w-1/4"></span>
               </div>
 
-              <form className="mt-10">
+              <form className="mt-10" onSubmit={handleSubmit}>
                 <LoginInput
                   type="text"
                   label="Username"
                   className="my-6"
-                  isInvalid={error}
-                  onChange={setUsername}
+                  isInvalid={errors}
+                  onChange={setUsernameChange}
                   id={"email"}
                 />
 
@@ -60,21 +96,28 @@ export default function LoginPage() {
                   type="password"
                   label="Contraseña"
                   className="my-6"
-                  isInvalid={error}
-                  onChange={setPassword}
+                  isInvalid={errors}
+                  onChange={setPasswordChange}
                   id={"password"}
                 />
 
-
-                <Alert error={error} label="No se pudo iniciar sesion con las credenciales
-                        proporcionadas." />
+                <Alert
+                  error={errors}
+                  label="No se pudo iniciar sesion con las credenciales
+                        proporcionadas."
+                />
 
                 <div
-                  className={`${error ? "hidden" : "block"} h-16 mt-10`}
+                  className={`${errors ? "hidden" : "block"} h-16 mt-10`}
                 ></div>
 
                 <div className=" mt-10  flex justify-center items-center ">
-                  <LoginSubmit color="secondary" variant="shadow" label="Iniciar Sesion"/>
+                  <LoginSubmit
+                    color="secondary"
+                    variant="shadow"
+                    label="Iniciar Sesion"
+                    isLoading={loading}
+                  />
                 </div>
               </form>
             </div>
