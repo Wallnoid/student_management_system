ba04de6931e15b37e5b994f552073dee72bfc8f9
EmeritusThinkPad <jairparedesal123@gmail.com>
2024-06-22 15:08:55 -0500

    form and tabs on modal

--- src/app/(platform)/finanzas/components/main-container/actions_row.tsx
+++ src/app/(platform)/finanzas/components/main-container/actions_row.tsx
@@ -1,15 +1,22 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { FaQuestion } from 'react-icons/fa';
 import { GiPodiumWinner, GiPublicSpeaker } from "react-icons/gi";
 import { usePayments } from '../../paymentsContext';
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, DatePicker, Input, Textarea, DateValue } from "@nextui-org/react";
 import { Card, CardBody } from "@nextui-org/react";
+import { Tabs, Tab } from "@nextui-org/react";
 import { Payments } from '@/interfaces/Payments';
 import { Participation } from '@/interfaces/Participation';
 import { Contest } from '@/interfaces/Contest';
 import { Event } from '@/interfaces/Event';
 import { Team } from '@/interfaces/Team';
+import { useFormik } from 'formik';
+import toast from 'react-hot-toast';
+import { addPyment } from '@/services/payments.service';
+import SelectUsers from '@/app/(platform)/tareas/components/selectUsers';
+import { parseDate } from '@/utils/utils';
+import { currentDate } from '@/constants/date_constants';
 
 export default function ActionsRow() {
   const { contestPayments, eventPayments, variousPayments } = usePayments();
@@ -59,6 +66,37 @@ export default function ActionsRow() {
 
 function ContestModalInfo({ payments }: { payments: Payments[] }) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const formik = useFormik({
+    initialValues: {
+      monto: "",
+      detalle: "",
+      categoria: "",
+      user: ""
+    },
+    onSubmit: (values) => {
+      const payment: Payments = {
+        monto: parseFloat(values.monto),
+        tipo: "egreso",
+        detalle: values.detalle,
+        categoria: values.categoria,
+        creado_por: values.user,
+        fecha_hora_creacion: new Date().toISOString()
+      };
+      toast.promise(addPyment(payment, 'concursos'), {
+        loading: "Guardando...",
+        success: () => {
+          console.log("Pago registrado correctamente!");
+          formik.resetForm();
+          return <b>Pago Registrado</b>;
+        },
+        error: (err) => {
+          formik.setSubmitting(false);
+          return `${err.message.toString()}`;
+        },
+      });
+    },
+  });
+
   return (
     <div>
       <Button className='hidden' onPress={onOpen} id={'modal_contest_button'}>Open Modal</Button>
@@ -66,39 +104,73 @@ function ContestModalInfo({ payments }: { payments: Payments[] }) {
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">Transacciones en Concursos</ModalHeader>
-              <ModalBody>
-                {
-                  payments?.map((payment, index) => (
-                    <Card key={index} className="mb-2">
-                      <CardBody>
-                        <div className="text-gray-800 text-sm space-y-2">
-                          <div className="flex justify-between items-center p-2">
-                            <p><strong>Monto:</strong> ${payment.monto.toFixed(2)}</p>
-                            <div className="bg-secondary rounded-full px-3 rounded-md text-white">
-                              <p>{payment.tipo.toUpperCase()}</p>
+              <Tabs aria-label="Options" className='w-full pt-12 justify-center'>
+                <Tab key="transactions_list" title="Lista de Transacciones">
+                  <ModalBody>
+                    {
+                      payments?.map((payment, index) => (
+                        <Card key={index} className="mb-2">
+                          <CardBody>
+                            <div className="text-gray-800 text-sm space-y-2">
+                              <div className="flex justify-between items-center p-2">
+                                <p><strong>Monto:</strong> ${payment.monto.toFixed(2)}</p>
+                                <div className={`rounded-full px-3 rounded-md text-white ${payment.tipo == 'ingreso' ? 'bg-secondary' : 'bg-error'}`}>
+                                  <p>{payment.tipo.toUpperCase()}</p>
+                                </div>
+                              </div>
+                              <div className="flex justify-between items-center px-2">
+                                <p><strong>Concurso:</strong> {String(((payment.id_participacion as Participation).id_concurso as Contest).nombre)}</p>
+                              </div>
+                              <div className="flex justify-between items-center px-2">
+                                <p><strong>Equipo: </strong>{String(((payment.id_participacion as Participation).id_equipo as Team).nombre)}</p>
+                              </div>
                             </div>
-                          </div>
-                          <div className="flex justify-between items-center px-2">
-                            <p><strong>Concurso:</strong> {String(((payment.id_participacion as Participation).id_concurso as Contest).nombre)}</p>
-                          </div>
-                          <div className="flex justify-between items-center px-2">
-                            <p><strong>Equipo:</strong>{String(((payment.id_participacion as Participation).id_equipo as Team).nombre)}</p>
-                          </div>
-                        </div>
-                      </CardBody>
-                    </Card>
-                  ))
-                }
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Cancelar
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Aceptar
-                </Button>
-              </ModalFooter>
+                          </CardBody>
+                        </Card>
+                      ))
+                    }
+                    <ModalFooter className='flex justify-center'>
+                      <Button color="danger" onPress={onClose}>
+                        Cancelar
+                      </Button>
+                      <Button color="primary" onPress={onClose}>
+                        Aceptar
+                      </Button>
+                    </ModalFooter>
+                  </ModalBody>
+                </Tab>
+                <Tab key="add_outcomes" title="Agregar Egresos">
+                  <ModalBody>
+                    <form onSubmit={formik.handleSubmit}>
+                      <ModalBody>
+                        <Input
+                          autoFocus
+                          label="Monto"
+                          name="monto"
+                          onChange={formik.handleChange}
+                          value={formik.values.monto}
+                          placeholder="Ingresa el monto del pago"
+                        />
+                        <Textarea
+                          label="Detalle"
+                          name="detalle"
+                          onChange={formik.handleChange}
+                          value={formik.values.detalle}
+                          placeholder="Descripción del pago"
+                        />
+                      </ModalBody>
+                      <ModalFooter className='flex justify-center'>
+                        <Button color="danger" onPress={onClose}>
+                          Cancelar
+                        </Button>
+                        <Button color="primary" type="submit">
+                          Registrar Pago
+                        </Button>
+                      </ModalFooter>
+                    </form>
+                  </ModalBody>
+                </Tab>
+              </Tabs>
             </>
           )}
         </ModalContent>
